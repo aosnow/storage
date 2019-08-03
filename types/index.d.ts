549d6cd6f9b9589a46a052d2a49dbfa7963d66b8
张雄军 <zxj@blibao.cn>
2019-08-04 02:38:32 +0800

    类型定义调整

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------------------------
+// name: index.d
+// author: 喵大斯( mschool.tech )
+// created: 2019/8/4
+// ------------------------------------------------------------------------------
+
+// import Vue from 'vue';
+// import { EasyHttp } from '@mudas/http';
+//
+// export interface VueConstructor<V extends Vue = Vue>
+// {
+//   http:EasyHttp;
+// }
+//
+// export interface EasyHttp{}
+
+
+
